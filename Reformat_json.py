3e4165f07febbe1844db47941eca256d5660d961
yordyckean <32854826+yordyckean@users.noreply.github.com>
2022-03-22 22:36:21 -0400

    scrapy scraper commit

--- Reformat_json.py
+++ Reformat_json.py
@@ -1,13 +1,20 @@
 import json
 
-
-f = open('recipes_1_29.json','r')
-db=json.load(f)
+f = open('2-24.json', 'r')
+db = json.load(f)
 for ele in db[0:]:
+    if (len(ele) < 3):
+        db.remove(ele)
+    with open('Mar_recipes.json', 'w') as file:
+        json.dump(db, file, indent=4)
+"""for ele in db[0:]:
    if (len(ele)<2)==True:
        db.remove(ele)
-with open('Jan_recipe.json','w') as file:
+for Ingredients in 
+with open('recipes_scraper/spiders/Feb_recipe.json', 'w') as file:
     json.dump(db,file,indent=4)
+    
+    """
 
 
 #text in-between commans
